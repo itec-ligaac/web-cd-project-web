cd6af63ee58d9a4551583d5fb3ed72f5360e8031
danut07 <danny201312@gmail.com>
2021-03-27 18:14:51 +0200

    CovidDataPage

--- pandemic-holiday-frontend/src/components/CovidData/CovidDataTable.tsx
+++ pandemic-holiday-frontend/src/components/CovidData/CovidDataTable.tsx
@@ -1,8 +1,9 @@
-import { AutoComplete, Table } from "antd";
-import { useContext } from "react";
+import { AutoComplete, Input, Table } from "antd";
+import { useContext, useEffect, useState } from "react";
 import React from "react";
 //@ts-ignore
 import { CovidDataContext } from "./index.tsx";
+import styled from "styled-components";
 
 interface covidData {
   id: string;
@@ -11,8 +12,34 @@ interface covidData {
   Infection_Risk: number;
 }
 
+const TableHeader = styled.div`
+  justify-content: space-between;
+  flex-flow: row wrap;
+  margin-bottom: 10px;
+  min-width: 200px;
+  display: flex;
+`;
+
 export const CovidDataTable = () => {
+  const [filteredTableData, setFilteredTableData] = useState(null);
   const { covidTableData, setCovidTableData } = useContext(CovidDataContext);
+
+  useEffect(() => {
+    setFilteredTableData(covidTableData);
+  }, [covidTableData]);
+
+  const handleSearchChange = (event: any) => {
+    if (event.target.value === "") setFilteredTableData(covidTableData);
+    else
+      setFilteredTableData(
+        covidTableData.filter(
+          (element: covidData) =>
+            element.Country.toLowerCase().indexOf(
+              event.target.value.toLowerCase()
+            ) !== -1
+        )
+      );
+  };
 
   const columns = [
     {
@@ -37,14 +64,23 @@ export const CovidDataTable = () => {
 
   return (
     <>
+      <TableHeader>
+        <Input
+          onChange={handleSearchChange}
+          style={{ width: "200px", marginTop: "10px" }}
+          placeholder="Search Country"
+        />
+      </TableHeader>
       <Table
-        dataSource={covidTableData}
-        scroll={{ x: "max-content" }}
+        //@ts-ignore
+        dataSource={filteredTableData}
+        scroll={{ x: 200, y: "maximum-content" }}
         columns={columns}
         style={{
           marginLeft: 160,
           marginRight: 160,
           marginTop: 50,
+          scale: 20,
         }}
       />
     </>
