afa364f3079dff691d4cac7bbe7c3de2adc7d3a3
catalin.chita@cst.ro <catalin.chita@cst.ro>
2021-03-27 21:41:00 +0200

    added embedded websites links

--- pandemic-holiday-frontend/src/components/AppRouting/index.tsx
+++ pandemic-holiday-frontend/src/components/AppRouting/index.tsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 import { CovidData } from "../CovidData";
 import { TravelData } from "../TravelData";
 
@@ -51,6 +57,7 @@ export const AppRouting = () => {
               <Route path="/landingpage">
                 <LandingPage />
               </Route>
+              <Redirect to="/landingpage" />
             </Switch>
           </div>
         </Content>
