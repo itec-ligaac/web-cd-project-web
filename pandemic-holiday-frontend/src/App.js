6af8e362ab41ca44ffb9b66ba497436eb4dfb46e
catalin.chita@cst.ro <catalin.chita@cst.ro>
2021-03-27 16:55:34 +0200

    added city select

--- pandemic-holiday-frontend/src/App.js
+++ pandemic-holiday-frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React from "react";
 import { AppRouting } from "./components/AppRouting";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 
 function App() {
   return <AppRouting />;
--- pandemic-holiday-frontend/src/components/TravelData/index.tsx
+++ pandemic-holiday-frontend/src/components/TravelData/index.tsx
@@ -1,5 +1,72 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { Select, Card } from "antd";
+import Cities from "../city.list.json";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const { Option } = Select;
 
 export const TravelData = () => {
-  return <>travel data</>;
+  useEffect(() => {
+    const getWeather = async () => {
+      const response = await axios
+        .get(
+          "https://api.openweathermap.org/data/2.5/weather?q=London&appid=98d72ef0d8203ca4560b3295e1efa62a"
+        )
+        .then(function (response: any) {
+          console.log(response.data);
+        })
+        .catch(function (responseError: any) {
+          console.log(responseError.code);
+        });
+    };
+
+    getWeather();
+  }, []);
+
+  function handleSelectCountry(value: any) {
+    console.log(`selected ${value}`);
+  }
+
+  let cityArray = JSON.parse(JSON.stringify(Cities));
+
+  cityArray = cityArray.sort((a: any, b: any) => {
+    if (a.stat.population < b.stat.population) return 1;
+    else return -1;
+  });
+
+  console.log(cityArray);
+
+  return (
+    <Container>
+      <Card
+        style={{
+          width: "30vw",
+          height: "30vw",
+        }}
+      >
+        <Select
+          defaultValue="Find a city!"
+          style={{ width: 120 }}
+          onChange={handleSelectCountry}
+        >
+          {cityArray.map((city: any) => {
+            return (
+              <Option key={city.id} value={city.name}>
+                {city.name}
+              </Option>
+            );
+          })}
+        </Select>
+      </Card>
+    </Container>
+  );
 };
