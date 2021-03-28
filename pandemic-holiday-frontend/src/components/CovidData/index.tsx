1bd95d5dbb7d6910caa9c2e94ae2f28c13d1d9cc
catalin.chita@cst.ro <catalin.chita@cst.ro>
2021-03-28 02:31:11 +0200

    final touch

--- pandemic-holiday-frontend/src/components/CovidData/index.tsx
+++ pandemic-holiday-frontend/src/components/CovidData/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import { CovidDataTable } from "./CovidDataTable";
 import { useCovidData } from "./useCovidData";
 import styled from "styled-components";
+import useWindowWidth from "react-hook-use-window-width";
 
 const Container = styled.div`
   display: flex;
   align-items: flex-start;
+  justify-content: center;
+  flex-wrap: wrap;
 `;
 
 // @ts-ignore
@@ -13,11 +16,17 @@ export const CovidDataContext = React.createContext();
 
 export const CovidData = () => {
   const { covidTableData, setCovidTableData } = useCovidData();
-
+  const width = useWindowWidth();
   return (
     <CovidDataContext.Provider value={{ covidTableData, setCovidTableData }}>
       <Container>
-        <div style={{ width: "60%", margin: "20px", height: "87vh" }}>
+        <div
+          style={{
+            flexGrow: 3,
+            margin: "20px",
+            height: "87vh",
+          }}
+        >
           <iframe
             style={{ height: "87vh", width: "100%" }}
             src="https://app.developer.here.com/coronavirus/"
@@ -31,10 +40,11 @@ export const CovidData = () => {
         </div>
         <div
           style={{
-            width: "40%",
+            flexGrow: 1,
             margin: "20px",
             maxHeight: "100%",
             overflow: "hidden",
+            width: width > 500 ? "30%" : "100%",
           }}
         >
           <CovidDataTable />
